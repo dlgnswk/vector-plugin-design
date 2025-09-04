09c0c78f2e5948288794d7dec57e676ab0619059
Jaehoon Lee <dlwogns5140@gmail.com>
2025-09-04 14:59:12 +0900

    chore: gitignore에 dist 캐시 삭제후 폴더 지정

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-
-type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
-    variant?: "primary" | "icon";
-};
-declare const Button: React.FC<ButtonProps>;
-
-export { Button, type ButtonProps };

5b8f315d96648de65aae4a6377c85a76e7005c84
Jaehoon Lee <dlwogns5140@gmail.com>
2025-09-05 17:10:20 +0900

    feat: ItemContainer 컴포넌트에 className prop 추가

--- src/components/ItemContainer.tsx
+++ src/components/ItemContainer.tsx
@@ -4,15 +4,21 @@ import { cn } from "../util";
 interface ItemContainerProps {
   children: React.ReactNode;
   type: "col" | "row";
+  className?: string;
 }
 
-export const ItemContainer = ({ children, type }: ItemContainerProps) => {
+export const ItemContainer = ({
+  children,
+  type,
+  className,
+}: ItemContainerProps) => {
   return (
     <div
       className={cn(
         "vd-flex vd-items-center vd-w-full vd-gap-2",
         type === "col" && "vd-flex-col vd-gap-0",
-        type === "row" && "vd-flex-row vd-gap-2 vd-px-4 vd-py-[5px]"
+        type === "row" && "vd-flex-row vd-gap-2 vd-px-4 vd-py-[5px]",
+        className
       )}
     >
       {children}
